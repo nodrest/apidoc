6585d46e83a781de71a47bc4122c372efa52fcd5
大鼻子 <wohugb@gmail.com>
2014-03-14 13:43:34 +0800

    Update zh.js
    "Generated with"              : "生成工具",

--- template/locales/zh.js
+++ template/locales/zh.js
@@ -7,7 +7,7 @@ define({
 		"Description"                 : "描述",
 		"Field"                       : "字段",
 		"General"                     : "一般",
-		"Generated with"              : "使用产生",
+		"Generated with"              : "生成工具",
 		"Name"                        : "名称",
 		"No response values."         : "无响应值.",
 		"optional"                    : "选项",
