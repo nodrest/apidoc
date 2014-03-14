e9c190572efd736cd3546463f0b3700004e42fd5
大鼻子 <wohugb@gmail.com>
2014-03-14 13:39:30 +0800

    Create zh.js

---
+++ template/locales/zh.js
@@ -0,0 +1,19 @@
+define({
+	"zh": {
+		"Compare all with predecessor": "使用预处理比较", 
+		"compare changes to:"         : "比较变跟:",
+		"compared to"                 : "比较",
+		"Default value:"              : "默认值:",
+		"Description"                 : "描述",
+		"Field"                       : "字段",
+		"General"                     : "一般",
+		"Generated with"              : "使用产生",
+		"Name"                        : "名称",
+		"No response values."         : "无响应值.",
+		"optional"                    : "选项",
+		"Parameter"                   : "参数",
+		"Permission:"                 : "允许:",
+		"show up to version:"         : "显示到版本:",
+		"Type"                        : "类型"
+	}
+});
